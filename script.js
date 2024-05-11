5a68b0890aa293f058935e2437b0878e4a52d78b
Arun8072 <atunviknezh999@gmail.com>
2024-05-11 17:27:24 +0530

    little changes

--- script.js
+++ script.js
@@ -68,3 +68,17 @@ contactChips.forEach((chip) => {
     closeButton.addEventListener('click', closeModal);
   });
 });
+
+//show more
+const showMoreBtn = document.getElementById('show-more-btn');
+const additionalProjects = document.querySelector('.additional-projects');
+
+showMoreBtn.addEventListener('click', () => {
+  additionalProjects.classList.toggle('hidden');
+  if (additionalProjects.classList.contains('hidden')) {
+    showMoreBtn.textContent = 'Show More';
+  } else {
+    showMoreBtn.textContent = 'Show Less';
+  }
+});
+
