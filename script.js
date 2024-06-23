d4d3835a30e03e84eb5c2387221ea25fdcd9d825
Arun8072 <atunviknezh999@gmail.com>
2024-06-23 18:01:40 +0530

    added show all feature

--- script.js
+++ script.js
@@ -70,16 +70,36 @@ contactChips.forEach((chip) => {
 });
 
 // Show/Hide additional courses
-const showMoreBtn = document.getElementById('show-more-btn');
+const showMoreBtnC = document.getElementById('showMore-course');
 const additionalCourses = document.querySelector('.additional-courses');
-const courseCards = document.getElementById('course-cards');
+//const courseCards = document.getElementById('course-cards');
 
-showMoreBtn.addEventListener('click', () => {
+showMoreBtnC.addEventListener('click', () => {
   additionalCourses.classList.remove('d-none');
   if (additionalCourses.classList.contains('d-none')) {
-    showMoreBtn.textContent = 'Show More';
+    showMoreBtnC.textContent = 'Show More';
   } else {
-    /*add another  full course section or modal to display aLL
-    showMoreBtn.textContent = 'Show All'; */
+    /* add another  full course section or modal to display aLL
+    showMoreBtnC.textContent = 'Show All'; */
+  }
+});
+
+// Show/Hide additional projects
+const showMoreBtnP = document.getElementById('showMore-projects');
+const additionalProjects = document.getElementById('additional-projects');
+const allProjects = document.getElementById('all-projects');
+
+showMoreBtnP.addEventListener('click', () => {
+
+  if (additionalProjects.classList.contains('d-none') && allProjects.classList.contains('d-none') ) {
+    additionalProjects.classList.remove('d-none');
+    showMoreBtnP.textContent = 'Show All';
+  } else if (allProjects.classList.contains('d-none') && !additionalProjects.classList.contains('d-none')){
+    allProjects.classList.remove('d-none');
+    showMoreBtnP.textContent = 'Show Less';
+  }else if (!additionalProjects.classList.contains('d-none') && !allProjects.classList.contains('d-none')){
+    additionalProjects.classList.add('d-none');
+    allProjects.classList.add('d-none'); 
+    showMoreBtnP.textContent = 'Show More';
   }
 });
